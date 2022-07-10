8a4e88fd69071032a021d0219da203bce6e2d99f
GEEKBRO00 <108108712+GEEKBRO00@users.noreply.github.com>
2022-07-10 02:44:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+2349033807504']
+global.premium =['+2349033807504']
+global.ownernomer = '+2349033807504'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
